419063d79d4b7f4bf0cf726611c1e4595536cb54
Malcolm Morgan <mem48@users.noreply.github.com>
2018-11-21 15:08:48 +0000

    first commit

---
+++ R/reports.R
@@ -0,0 +1,93 @@
+#' Get reports
+#'
+#' @param sites Number of the WebTRIS site e.g. 8188
+#' @param period Time period of reports can be "monthly" or "daily". Default "daily"
+#' @param start_date Start date as object of class Date. Default Today - 60 days
+#' @param end_date End date as object of class Date. Default Today
+#'
+#' @export
+#'
+#' @details
+#' This function returns a report for the given WebTRIS site based
+#'
+WebTRIS_reports <- function(sites = NA,
+                     period = "daily",
+                     start_date = Sys.Date() - 60,
+                     end_date = Sys.Date(),
+                    )
+{
+  # Check Valid Inputs
+  api <- "v1.0"
+  checkmate::assert_numeric(sites, lower =  0, upper = 100000, len = 1)
+  checkmate::assert_subset(period, choices = c("daily","monthly","annual"), empty.ok = F)
+  checkmate::assert_date(start_date, upper = Sys.Date())
+  checkmate::assert_date(end_date, lower = start_date, upper = Sys.Date())
+  start_date <- as.character(format(start_date, "%d%m%Y"))
+  end_date <-  as.character(format(end_date, "%d%m%Y"))
+
+
+  #Get Data function
+  get_data <- function(page, sites, start_date, end_date){
+    reqUrl = paste0("http://webtris.highwaysengland.co.uk/api/",api,"/reports/",period)
+    # Construct URL
+    req <- httr::GET(
+      reqUrl,
+      query = list(
+        sites = sites,
+        start_date = start_date,
+        end_date = end_date,
+        page = page,
+        page_size = 40000
+      )
+    )
+
+    # convert response content into text
+    text <- httr::content(req, as = "text", encoding = "UTF-8")
+
+    # Check for no data returned
+    if(nchar(text) < 100){
+      if(text == ""){
+        message("No data returned for that site and time period")
+        stop()
+      }
+      if(grepl('Report Request Invalid',text_fail)){
+        message("Invalid request")
+        stop()
+      }
+    }
+
+    # parse text to json
+    asjson <- jsonlite::fromJSON(text)
+
+    if(page == 1){
+      return(asjson)
+    }else{
+      return(asjson$Rows)
+    }
+
+  }
+
+  result <- get_data(1, sites, start_date, end_date)
+  res_header <- result$Header
+  res_data <- result$Rows
+
+
+  # Check if multiple requests needed
+  if(res_header$row_count > nrow(res_data)){
+    nreq = ceiling(res_header$row_count / 40000)
+    res_list <- lapply(seq(2,nreq),get_data, sites = sites, start_date = start_date, end_date = end_date)
+    res_data <- list(res_data)
+    res_list <- c(res_data, res_list)
+    res_data <- dplyr::bind_rows(res_list)
+    rm(res_list)
+  }
+
+
+  # Format the Data
+  res_data$DateTime_Ending <- lubridate::ymd_hms(paste0(substr(res_data$`Report Date`,1,10)," ",res_data$`Time Period Ending`))
+  res_data <- res_data[,c("Site Name","DateTime_Ending","Time Interval",
+                         names(res_data)[!names(res_data) %in% c("Site Name","DateTime_Ending","Time Interval","Report Date","Time Period Ending")])]
+  res_data[,3:23] <- lapply(res_data[,3:23], as.numeric)
+
+  return(res_data)
+}
---
+++ R/sites.R
@@ -0,0 +1,55 @@
+#' Get sites
+#'
+#' @param sites Number vector of site ids or NA for all sites
+#'
+#' @export
+#'
+#' @details
+#' This function returns sites data for the given WebTRIS site based
+#'
+WebTRIS_sites <- function(sites = NA, clean = TRUE)
+{
+  # Check Valid Inputs
+  api <- "v1.0"
+  checkmate::assert_numeric(sites)
+
+  # Construct URL
+  if(is.na(sites)){
+    reqUrl = paste0("http://webtris.highwaysengland.co.uk/api/",api,"/sites")
+  }else{
+    reqUrl = paste0("http://webtris.highwaysengland.co.uk/api/",api,"/sites/",paste(sites,collapse = ","))
+  }
+
+  # Get Data
+  req <- httr::GET(reqUrl)
+  text <- httr::content(req, as = "text", encoding = "UTF-8")
+  asjson <- jsonlite::fromJSON(text)
+
+  res_header <- asjson$row_count
+  res_data <- asjson$sites
+
+
+  return(res_data)
+}
+
+#' Get Cleans Site Names
+#' The Name field contains useful data such as the direction of flow, this function extracts this data.
+#'
+#' @param x Data.Frame from WebTRIS_sites()
+#'
+#' @export
+#'
+#' @details
+#'
+#'
+WebTRIS_clean_sites <- function(x){
+  nms = strsplit(x$Name,";")
+  nms1 = sapply(nms, function(l){if(length(l)>0){l[[1]]}else{NA}} )
+  #nms2 = sapply(nms, function(l){if(length(l)>1){l[[2]]}else{NA}} )
+  #nms3 = sapply(nms, function(l){if(length(l)>2){l[[3]]}else{NA}} )
+  nms4 = sapply(nms, function(l){if(length(l)>3){l[[4]]}else{NA}} )
+
+  x$direction = as.factor(gsub(" ","",nms4))
+  x$name = nms1
+  return(x)
+}
